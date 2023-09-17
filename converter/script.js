9f1edf6c675acb4aabb62bad4d9bbb9b38d9e20d
Andrew Lapteff <this.is.amigogo@gmail.com>
2023-09-17 15:31:14 +0300

    feat: Implement color replacement feature

--- converter/script.js
+++ converter/script.js
@@ -5,14 +5,20 @@ const downloadLink = document.getElementById('downloadLink')
 const fileNameInput = document.getElementById('fileNameInput')
 const widthInp = document.getElementById('width')
 const heightInp = document.getElementById('height')
+const targetColorInp = document.getElementById('targetColor')
+const replacementColorInp = document.getElementById('replacementColor')
+const shouldDownloadBtn = document.getElementById('shouldDownload')
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
 convertButton.addEventListener('click', () => {
+  const targetColor = targetColorInp.value
+  const replacementColor = replacementColorInp.value
   const selectedFormat = outputFormat.value
   const width = widthInp.value
   const height = heightInp.value
-  const fileName = fileNameInput.value
+  const shouldDownload = shouldDownloadBtn.checked
+  const fileName = fileNameInput.value || 'image'
   const selectedFile = fileInput.files[0]
 
   if (selectedFile) {
@@ -25,28 +31,79 @@ convertButton.addEventListener('click', () => {
       img.src = reader.result
 
       img.onload = function () {
-        let new_width, new_height
-        if (width != 0) {
-          new_width = width
-          new_height = (new_width / img.width) * img.height
-        } else {
-          new_height = height
-          new_width = (new_height / img.height) * img.width
-        }
+        const { new_width, new_height } = setSize(width, img, height)
+
         canvas.height = new_height
         canvas.width = new_width
         ctx.drawImage(img, 0, 0, new_width, new_height)
+
+        colorChanger(ctx, canvas, targetColor, replacementColor)
 
         canvas.style.display = 'block'
 
         const dataURL = canvas.toDataURL(`image/${selectedFormat}`) // 'image/img', 'image/webp', 'image/png'
 
-        const a = document.createElement('a')
-        a.href = dataURL
-        console.log(fileName)
-        a.download = `${fileName}.${selectedFormat}`
-        a.click()
+        if (shouldDownload) {
+          downloadImage(dataURL, fileName, selectedFormat)
+        }
       }
     }
   }
 })
+
+function setSize(width, img, height) {
+  let new_width, new_height
+  if (width != 0) {
+    new_width = width
+    new_height = (new_width / img.width) * img.height
+  } else if (height != 0) {
+    new_height = height
+    new_width = (new_height / img.height) * img.width
+  } else {
+    new_height = img.height
+    new_width = img.width
+  }
+  return { new_width, new_height }
+}
+
+function colorChanger(ctx, canvas, targetColor, replacementColor) {
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+  const data = imageData.data
+
+  const targetColorRGB = hexToRGB(targetColor)
+  const replacementColorRGB = hexToRGB(replacementColor)
+
+  for (let i = 0; i < data.length; i += 4) {
+    const pixelColor = [data[i], data[i + 1], data[i + 2]]
+    if (compareColors(pixelColor, targetColorRGB)) {
+      data[i] = replacementColorRGB[0]
+      data[i + 1] = replacementColorRGB[1]
+      data[i + 2] = replacementColorRGB[2]
+    }
+  }
+
+  ctx.putImageData(imageData, 0, 0)
+}
+
+function hexToRGB(hex) {
+  const bigint = parseInt(hex.slice(1), 16)
+  const r = (bigint >> 16) & 255
+  const g = (bigint >> 8) & 255
+  const b = bigint & 255
+  return [r, g, b]
+}
+
+function compareColors(color1, color2) {
+  return (
+    color1[0] === color2[0] &&
+    color1[1] === color2[1] &&
+    color1[2] === color2[2]
+  )
+}
+
+function downloadImage(dataURL, fileName, selectedFormat) {
+  const a = document.createElement('a')
+  a.href = dataURL
+  a.download = `${fileName}.${selectedFormat}`
+  a.click()
+}
