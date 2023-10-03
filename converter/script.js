4a201da22aa444a905d13c8c81c26b6526a82ba9
Andrew Lapteff <this.is.amigogo@gmail.com>
2023-10-03 12:16:08 +0300

    feat: merging two images feat: watermark

--- converter/script.js
+++ converter/script.js
@@ -1,4 +1,5 @@
 const fileInput = document.getElementById('imageInput')
+const fileInput2 = document.getElementById('imageInput2')
 const outputFormat = document.getElementById('outputFormat')
 const convertButton = document.getElementById('convertButton')
 const downloadLink = document.getElementById('downloadLink')
@@ -19,6 +20,12 @@ const cropYInput = document.getElementById('cropY')
 const cropWidthInput = document.getElementById('cropWidth')
 const cropHeightInput = document.getElementById('cropHeight')
 const contrastInput = document.getElementById('contrastInput')
+const numClonesInput = document.getElementById('numClones')
+const mergeDirection = document.getElementById('mergeDirection')
+const watermarkText = document.getElementById('watermarkText')
+const opacityInput = document.getElementById('opacity')
+const textColorInput = document.getElementById('textColor')
+const fontScaleInput = document.getElementById('fontScale')
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
@@ -40,52 +47,153 @@ convertButton.addEventListener('click', () => {
   const cropY = parseInt(cropYInput.value)
   const cropWidth = parseInt(cropWidthInput.value)
   const cropHeight = parseInt(cropHeightInput.value)
-  const selectedFile = fileInput.files[0]
   const contrast = contrastInput.value
+  const numClones = numClonesInput.value
+  const selectedFile = fileInput.files[0]
+  const selectedFile2 = fileInput2.files[0]
+  const text = watermarkText.value
+  const opacity = opacityInput.value / 100
+  const textColor = textColorInput.value
+  const direction = mergeDirection.value
+  const fontScale = fontScaleInput.value
+
   if (selectedFile) {
     const reader = new FileReader()
 
     reader.readAsDataURL(selectedFile)
 
     reader.onload = function () {
-      const img = new Image()
+      let img = new Image()
+      let img2 = new Image()
       img.src = reader.result
 
       img.onload = function () {
+        cloneAndPositionImagesOnCanvas(ctx, canvas, img, numClones)
         const { new_width, new_height } = setSize(width, img, height)
 
         canvas.height = new_height
         canvas.width = new_width
 
-        ctx.globalAlpha = transparency
+        if (selectedFile2) {
+          const reader2 = new FileReader()
+          reader2.readAsDataURL(selectedFile2)
+          reader2.onload = function () {
+            img2.src = reader2.result
+            if (direction === 'horizontal') {
+              canvas.width = img.width + img2.width
+              canvas.height = Math.max(img.height, img2.height)
+              ctx.drawImage(img, 0, 0)
+              ctx.drawImage(img2, img.width, 0)
+              methods(
+                targetColor,
+                replacementColor,
+                redCorrection,
+                greenCorrection,
+                blueCorrection,
+                numClones,
+                img,
+                colSplit,
+                rowSplit,
+                cropX,
+                cropY,
+                cropWidth,
+                cropHeight,
+                contrast,
+                selectedFormat,
+                shouldDownload,
+                fileName,
+                transparency,
+                text,
+                opacity,
+                textColor,
+                fontScale
+              )
+            } else if (direction === 'vertical') {
+              canvas.width = Math.max(img.width, img2.width)
+              canvas.height = img.height + img2.height
+              ctx.drawImage(img, 0, 0)
+              ctx.drawImage(img2, 0, img.height)
+            }
+          }
+        } else {
+          ctx.drawImage(img, 0, 0, new_width, new_height)
 
-        ctx.drawImage(img, 0, 0, new_width, new_height)
-
-        colorChanger(ctx, canvas, targetColor, replacementColor)
-
-        colorCorrection(
-          ctx,
-          canvas,
-          redCorrection,
-          greenCorrection,
-          blueCorrection
-        )
-
-        crop(img, colSplit, rowSplit, cropX, cropY, cropWidth, cropHeight)
-
-        contrastImage(canvas, contrast)
-
-        canvas.style.display = 'block'
-
-        const dataURL = canvas.toDataURL(`image/${selectedFormat}`) // 'image/img', 'image/webp', 'image/png'
-
-        if (shouldDownload) {
-          downloadImage(dataURL, fileName, selectedFormat)
+          methods(
+            targetColor,
+            replacementColor,
+            redCorrection,
+            greenCorrection,
+            blueCorrection,
+            numClones,
+            img,
+            colSplit,
+            rowSplit,
+            cropX,
+            cropY,
+            cropWidth,
+            cropHeight,
+            contrast,
+            selectedFormat,
+            shouldDownload,
+            fileName,
+            transparency,
+            text,
+            opacity,
+            textColor,
+            fontScale
+          )
         }
       }
     }
   }
 })
+
+function methods(
+  targetColor,
+  replacementColor,
+  redCorrection,
+  greenCorrection,
+  blueCorrection,
+  numClones,
+  img,
+  colSplit,
+  rowSplit,
+  cropX,
+  cropY,
+  cropWidth,
+  cropHeight,
+  contrast,
+  selectedFormat,
+  shouldDownload,
+  fileName,
+  transparency,
+  text,
+  opacity,
+  textColor,
+  fontScale
+) {
+  ctx.globalAlpha = transparency
+
+  colorChanger(ctx, canvas, targetColor, replacementColor)
+
+  colorCorrection(ctx, canvas, redCorrection, greenCorrection, blueCorrection)
+
+  if (numClones) cloneAndPositionImagesOnCanvas(ctx, canvas, img, numClones)
+
+  crop(img, colSplit, rowSplit, cropX, cropY, cropWidth, cropHeight)
+
+  contrastImage(canvas, contrast)
+
+  watermark(opacity, textColor, fontScale, text)
+
+  canvas.style.display = 'block'
+
+  const dataURL = canvas.toDataURL(`image/${selectedFormat}`) // 'image/img', 'image/webp', 'image/png'
+
+  if (shouldDownload) {
+    downloadImage(dataURL, fileName, selectedFormat)
+  }
+}
 
 function setSize(width, img, height) {
   let new_width, new_height
@@ -105,7 +213,6 @@ function setSize(width, img, height) {
 function colorChanger(ctx, canvas, targetColor, replacementColor) {
   const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
   const data = imageData.data
-
   const targetColorRGB = hexToRGB(targetColor)
   const replacementColorRGB = hexToRGB(replacementColor)
 
@@ -164,39 +271,57 @@ function downloadImage(dataURL, fileName, selectedFormat) {
 }
 
 function crop(img, colSplit, rowSplit, cropX, cropY, cropWidth, cropHeight) {
-  const imageWidth = img.width // Отримання ширини завантаженого зображення
-  const imageHeight = img.height // Отримання висоти завантаженого зображення
-  const partWidth = imageWidth / colSplit // Розрахунок ширини кожної частини зображення
-  const partHeight = imageHeight / rowSplit // Розрахунок висоти кожної частини зображення
-
+  const imageWidth = img.width
+  const imageHeight = img.height
+  const partWidth = imageWidth / colSplit
+  const partHeight = imageHeight / rowSplit
   for (let row = 0; row < rowSplit; row++) {
-    // Цикл для рядків
     for (let col = 0; col < colSplit; col++) {
-      // Цикл для стовпців
-      const partX = col * partWidth // Обчислення X-координати поточної частини
-      const partY = row * partHeight // Обчислення Y-координати поточної частини
-      // Видалення обраної області в поточній частині зображення
+      const partX = col * partWidth
+      const partY = row * partHeight
       ctx.clearRect(partX + cropX, partY + cropY, cropWidth, cropHeight) // x, y, width, height
     }
   }
 }
 
-function contrastImage(canvas, contrast) {
-  // Отримуємо дані пікселів з канвасу
-  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
-  // Отримуємо масив даних пікселів (червоний, зелений, синій, альфа)
-  var d = imageData.data
-  // Коригуємо значення контрастності
-  contrast = contrast / 100 + 1 // Конвертуємо у десятковий формат та зміщуємо діапазон: [0..2]
-  // Обчислюємо поправку для контрасту
-  var intercept = 128 * (1 - contrast)
-  // Проходимо по кожному пікселю
-  for (var i = 0; i < d.length; i += 4) {
-    // Застосовуємо корекцію контрастності до червоного, зеленого та синього каналів
-    d[i] = d[i] * contrast + intercept // Червоний канал
-    d[i + 1] = d[i + 1] * contrast + intercept // Зелений канал
-    d[i + 2] = d[i + 2] * contrast + intercept // Синій канал
+function cloneAndPositionImagesOnCanvas(ctx, canvas, img, numClones) {
+  const imageWidth = img.width
+  const imageHeight = img.height
+
+  const totalCloneWidth = imageWidth * numClones
+  const scaleFactor = canvas.width / totalCloneWidth
+  const cloneWidth = imageWidth * scaleFactor
+  const cloneHeight = imageHeight * scaleFactor
+
+  canvas.width = totalCloneWidth
+  canvas.height = cloneHeight
+
+  for (let i = 0; i < numClones; i++) {
+    const cloneX = imageWidth * i * scaleFactor
+    const cloneY = 0
+
+    ctx.drawImage(img, cloneX, cloneY, cloneWidth, cloneHeight)
   }
-  // Перевантажуємо дані зміненими даними пікселів на канвас
+}
+
+function contrastImage(canvas, contrast) {
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+
+  var d = imageData.data
+  contrast = contrast / 100 + 1 // Конвертуємо у десятковий формат та зміщуємо діапазон: [0..2]
+
+  var intercept = 128 * (1 - contrast)
+  for (var i = 0; i < d.length; i += 4) {
+    d[i] = d[i] * contrast + intercept
+    d[i + 1] = d[i + 1] * contrast + intercept
+    d[i + 2] = d[i + 2] * contrast + intercept
+  }
   ctx.putImageData(imageData, 0, 0)
+}
+
+function watermark(opacity, textColor, fontScale, text) {
+  ctx.globalAlpha = opacity
+  ctx.fillStyle = textColor
+  ctx.font = `${fontScale}px Arial`
+  ctx.fillText(text, 20, canvas.height - 20)
 }
